f291d9d54c36f84724697faabf9aeb005aced6f5
Calleemshotem <jayswmi242@gmail.com>
2026-05-15 15:49:48 -0500

    fixed playlist view navigation

--- client/src/api.js
+++ client/src/api.js
@@ -1,21 +1,19 @@
 import axios from 'axios';
 
-const API_BASE = 'https://scatapp-production.up.railway.app/api';
-
 export const api = {
   // Tracks
-  getTracks: (search = '') => axios.get(`${API_BASE}/tracks`, { params: { search } }),
-  uploadTrack: (formData) => axios.post(`${API_BASE}/tracks/upload`, formData, {
+  getTracks: (search = '') => axios.get('https://scatapp-production.up.railway.app/api/tracks', { params: { search } }),
+  uploadTrack: (formData) => axios.post('https://scatapp-production.up.railway.app/api/tracks/upload', formData, {
     headers: { 'Content-Type': 'multipart/form-data' }
   }),
-  deleteTrack: (trackId) => axios.delete(`${API_BASE}/tracks/${trackId}`),
+  deleteTrack: (trackId) => axios.delete(`https://scatapp-production.up.railway.app/api/tracks/${trackId}`),
 
   // Playlists
-  getPlaylists: () => axios.get(`${API_BASE}/playlists`),
-  createPlaylist: (name) => axios.post(`${API_BASE}/playlists`, { name }),
-  deletePlaylist: (playlistId) => axios.delete(`${API_BASE}/playlists/${playlistId}`),
+  getPlaylists: () => axios.get('https://scatapp-production.up.railway.app/api/playlists'),
+  createPlaylist: (name) => axios.post('https://scatapp-production.up.railway.app/api/playlists', { name }),
+  deletePlaylist: (playlistId) => axios.delete(`https://scatapp-production.up.railway.app/api/playlists/${playlistId}`),
   addTrackToPlaylist: (playlistId, trackId) => 
-    axios.post(`${API_BASE}/playlists/${playlistId}/tracks`, { trackId }),
+    axios.post(`https://scatapp-production.up.railway.app/api/playlists/${playlistId}/tracks`, { trackId }),
   removeTrackFromPlaylist: (playlistId, trackId) =>
-    axios.delete(`${API_BASE}/playlists/${playlistId}/tracks/${trackId}`),
+    axios.delete(`https://scatapp-production.up.railway.app/api/playlists/${playlistId}/tracks/${trackId}`),
 };
